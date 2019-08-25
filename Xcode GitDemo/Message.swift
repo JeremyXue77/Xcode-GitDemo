95c1328a4e983fe0adc02925224a51a323a805e8
Jeremy Xue <gg831006@gmail.com>
2019-08-25 15:43:13 +0800

    Add Message.swift file.

---
+++ Xcode GitDemo/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Xcode GitDemo
+//
+//  Created by Jeremy Xue on 2019/8/25.
+//  Copyright © 2019 jeremyxue. All rights reserved.
+//
+
+import Foundation
+
+let message = "Hello, Git"
+
+
