ea5fdf8758985ea377b17f14d4827186a7b92a91
Jeremy Xue <gg831006@gmail.com>
2019-08-25 14:19:08 +0800

    Initial Commit

---
+++ Xcode GitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xcode GitDemo
+//
+//  Created by Jeremy Xue on 2019/8/25.
+//  Copyright © 2019 jeremyxue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
