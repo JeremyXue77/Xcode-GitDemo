80251107318a1de0501184d89473b3e72444f35b
Jeremy Xue <gg831006@gmail.com>
2019-08-29 00:18:07 +0800

    change view background color to red

--- Xcode GitDemo/ViewController.swift
+++ Xcode GitDemo/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print(message)
-        view.backgroundColor = .black
+        view.backgroundColor = .red
     }
 
 
